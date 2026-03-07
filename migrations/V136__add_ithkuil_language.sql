cef3e891986773430ec9ee3a6c28774d98b2bd67
redusas <bangu@lojban.org>
2026-03-07 14:55:54 +0200

    feat: Add Ithkuil language to the database.

---
+++ migrations/V136__add_ithkuil_language.sql
@@ -0,0 +1,2 @@
+INSERT INTO languages (tag, englishname, lojbanname, realname, forlojban, url)
+VALUES ('art-ithkuil', 'Ithkuil', '.itku''le', 'wiosaḑca iţkuil', NULL, 'https://yuorb.github.io/en/');
